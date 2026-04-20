038f4207b6ffd633d2da0eab0ba3b45b39ff7ca8
Indian1999 <haszab99@gmail.com>
2026-04-20 18:39:46 +0200

    JELSZO checker

--- ora10(gyakorlas).py
+++ ora10(gyakorlas).py
@@ -2,7 +2,7 @@
 # g - kg
 # km - mi   (1 km = 0.62137 mi)
 # C° - F°   (F° = (C° * 1.8) + 32)
-
+"""
 print("Milyen mértékegységet szeretnél váltani?")
 print("1: gramm - kilogramm")
 print("2: kilométer - mérföld")
@@ -50,3 +50,68 @@ elif mode == "3":
         print("Érvénytelen menüpont")
 else:
     print("Érvénytelen menüpont!")
+
+
+# Számoljuk ki mennyi lesz az idő x perc múlva:
+# pl.: 17:57  +  100 perc -> 19:37
+
+time = input("Add meg a kezdeti időpontot (pl.: 17:30):")    # "14:32"
+duration = int(input("Hány perces legyen az időzítő? ")) # 100
+time = time.split(":") # ["14", "32"]
+time = int(time[0]) * 60 + int(time[1]) # 872
+end_time = time + duration # 972
+hour = end_time // 60
+days = hour // 24 # Hány nap múlva
+hour = hour % 24
+minute = end_time % 60
+print(f"{duration} perc elteltével ennyi lesz az idő: {days} nap múlva {hour}:{minute}")
+
+
+# Collatz-sejtés
+
+n = int(input("Add meg a kezdő számot a Collatz-sejtéshez: "))
+collatz = [n]
+while n != 1:
+    if n % 2 == 0: # páros
+        n //= 2
+    else:
+        n = n * 3 + 1
+    collatz.append(n)
+print(collatz)
+"""
+# Írjunk egy programot, amely leellenőrzi, hogy egy jelszó elég erős-e
+# Legyen benne kis és angybetű
+# Legyen benne szám és betű is
+# Legyen benne különleges karakter
+# Minimum 8, maximum 20 karakterből álljon
+
+password = input("Add meg a jelszavad: ")
+correct_length = len(password) >= 8 and len(password) <= 20
+has_lower = False
+has_upper = False
+has_digit = False
+has_special = False
+
+for char in password:
+    if char.islower():
+        has_lower = True
+    if char.isupper():
+        has_upper = True
+    if char in "0123456789":
+        has_digit = True
+    if char in ".,-;?!+_:<>#&@{}[]$×÷)(=/%)":
+        has_special = True
+
+if has_lower and has_upper and has_digit and has_special and correct_length:
+    print("A jelszó elég erős")
+else:
+    if not correct_length:
+        print("A jelszónak 8 és 20 karakter közöttinek kell lennie!")
+    if not has_lower:
+        print("A jelszóban szerepelnie kell kisbetűs karakternek!")
+    if not has_upper:
+        print("A jelszóban szereplnie kell nagybetűs karakternek!")
+    if not has_digit:
+        print("A jelszüban szereplnie kell számjegynek!")
+    if not has_special:
+        print("A jelszüban szerepelnie kell legalább 1 speciális karakternek!")
