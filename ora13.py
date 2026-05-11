5461922e7328270a48891c9ad9c80871ca79d9a8
Indian1999 <haszab99@gmail.com>
2026-05-11 18:00:32 +0200

    update

--- ora13.py
+++ ora13.py
@@ -1,29 +1,70 @@
-#Task 1 - find all the common values in the two lists
+#Task 1 - Szedjük ki egy 3. listába a két lista közös elemeit (metszet)
 list_1 = [61, 5, 6, 28, 87, 52, 89, 51, 86, 29, 93, 26, 99, 16, 36, 53, 47, 95, 18, 54, 62, 37, 34, 11, 75, 90, 88, 24, 72, 76, 55, 44,  3, 50, 35, 17, 94,  7, 31,100, 42, 43, 74, 83, 82,  4, 10]
 list_2 = [5, 9, 62, 79, 17, 68, 54, 50, 60, 89, 29, 41, 83, 77,  3, 86, 56, 13, 26, 52, 98, 81, 82, 74, 55, 66, 92, 61, 30, 37, 57, 91,  2, 71, 93, 35, 33, 24,100, 19, 65, 95, 90, 38, 88, 31, 80, 70, 25, 39, 15, 85, 42, 94, 11, 76, 32,  7, 48]
+metszet = []
+for item in list_1:
+    if item in list_2:
+        metszet.append(item)
 
+print(metszet)
 
-#Task 2 - create one number from the list values
-numbers = [12, 54, 812]
+#Task 2 - Fűzzük össze az elemeket 1 számmá
+numbers = [12, 54, 812] # 1254812
+num = ""
+for item in numbers:
+    num += str(item)
+num = int(num)
+print(num)
 
-# Task 3 - make two lists the same
+# Task 3 - A két lista legyen egyforma (ha az egyikből hiányzik egy elem ami a másokból nem, akkor adjuk hozzá)
 list_1 = [43, 70, 25, 39, 15, 85, 42, 94, 11, 76, 20,  36, 48]
 list_2 = [ 36, 44, 20, 96, 69, 15, 27, 14, 87, 67, 97, 43,  8, 22]
+for item in list_1:
+    if item not in list_2:
+        list_2.append(item)
 
+for item in list_2:
+    if item not in list_1:
+        list_1.append(item)
 
-# Task 4 - is the list sorted / is the list strictly increasing
-list = [43, 70, 25, 39, 15, 85, 42, 94, 11, 76, 20,  36, 48]
+# Helyben rendezi a listákat (növekvő sorrendbe)
+list_1.sort()
+list_2.sort()
+
+print(list_1)
+print(list_2)
 
 
 # Task 5 - price of items in a shop
 items = ["apple", "book", "bread", "cheese", "chicken", "curry sauce", "doughnut", "toilet roll", "socks", "toothpaste"]
 prices = [1500, 1000, 700, 1600, 1900, 600, 800, 999, 500, 550]
 
-# Task 6 - 100. Fibonacci number
+# Melyik az a termék amelyik a legolcsóbb? (és mennyibe kerül)
+min_index = 0
+for i in range(len(prices)):
+    if prices[i] < prices[min_index]:
+        min_index = i
+print(f"A legolcsóbb termék a '{items[min_index]}', {prices[min_index]} Ft-ba kerül")
 
-# Task 7 - 400m running results
-names = ["Bob", "Wanda", "Jared", "Emma", "Lisa", "Fred", "George", "Noah", "Rachel"]
-times = [123.42, 67.15, 80.70, 118.40, 99.95, 68.22, 71.51, 102.68, 80.88]
+# Sorold fel azokat a termékeket amelyek több mint 1000 forintba kerülnek (az árukkal együtt)
+print("Az 1000 Ft-tól drágább termékek:")
+for i in range(len(prices)):
+    if prices[i] > 1000:
+        print(f"{items[i]} - {prices[i]} Ft")
+
+# Írd ki, mennyit kell fizetni, ha veszek 3 zoknit, 1 sajtot, 2 kenyeret és 3 könyvet.
+kosar = ["socks", "socks", "socks", "cheese", "bread", "bread", "book", "book", "book"]
+vegosszeg = 0
+for item in kosar:
+    for i in range(len(items)):
+        if item == items[i]:
+            vegosszeg += prices[i]
+            break    # Kilépek a ciklusból
+
+print(f"A kosár tartalma: {kosar}")
+print(f"Végösszeg: {vegosszeg} Ft.")
+
+
 
 
 # Task 8 - price of the translation
@@ -59,3 +100,19 @@ age = [26, 31, 35, 41, 58, 30, 46, 61, 25]
 gender = ["male", "female", "male", "female", "female", "male", "male", "male", "female"]
 job = ["web developer", "marketing director", "content creator", "human resources", "CEO", "software developer", "public relations manager", "tester", "sales representative"]
 salary = [1500, 1500, 1400, 1300, 1400, 1500, 1400, 1300, 1500]
+
+
+
+
+
+
+
+
+# Task 4 - Ellenőrizzük le, hogy egy lista rendezett e (növekvő vagy csökkenő)
+list = [43, 70, 25, 39, 15, 85, 42, 94, 11, 76, 20,  36, 48]
+
+# Task 6 - 100. Fibonacci number
+
+# Task 7 - 400m running results
+names = ["Bob", "Wanda", "Jared", "Emma", "Lisa", "Fred", "George", "Noah", "Rachel"]
+times = [123.42, 67.15, 80.70, 118.40, 99.95, 68.22, 71.51, 102.68, 80.88]
