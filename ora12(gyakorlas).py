0ac6abece39c4d0a720631bfe5fa3a7ad154830c
Indian1999 <haszab99@gmail.com>
2026-05-11 17:19:21 +0200

    feladatok 13

--- ora12(gyakorlas).py
+++ ora12(gyakorlas).py
@@ -3,13 +3,14 @@
 
 lista = []
 while True:
+    break
     szam = input("Adj meg egy egész számot: ") # "32"
     if szam.isdigit():
         lista.append(int(szam))
     else:
         break
     print(lista)
-
+lista = [1,2,3,4,5]
 # Az elemek összegét
 összeg = 0
 for i in range(len(lista)):
@@ -21,8 +22,25 @@ atlag = összeg / len(lista)
 print(f"A lista elemeinek az átlaga: {round(atlag, 2)}")
 
 # Páros számok számát
+páros_darab = 0
+for i in range(len(lista)):
+    if lista[i] % 2 == 0:
+        páros_darab += 1
+print(f"Páros számok számára: {páros_darab}")
 
 # A legnagyobb/legkisebb elemet, terjedelmet ( max - min)
+max_index = 0
+min_index = 0
+for i in range(len(lista)):
+    if lista[i] > lista[max_index]:
+        max_index = i
+    if lista[i] < lista[min_index]:
+        min_index = i
+maximum = lista[max_index]
+minimum = lista[min_index]
+print(f"Minimum: {minimum}")
+print(f"Maximum: {maximum}")
+print(f"Terjedelem: {maximum - minimum}")
 
 # módusz
 max_index = 0
@@ -66,14 +84,35 @@ print(f"Alsó kvartilis: {median(first_half)}")
 print(f"Felső kvartilis: {median(second_half)}")
 
 
-# Task 2 - removing duplications from list
+# Feladat: Adott egy lista, ha egy elem többször szerepel a listában, akkor vegyük ki a duplikációt
 
-# Task 3 - extend command
+lista = [4, 9, 1, 2, 2, 1, 9, 8, 6, 7, 2, 3, 6, 3, 1, 2, 7, 9]
+halmaz = []
+for i in range(len(lista)):
+    if lista[i] not in halmaz:
+        halmaz.append(lista[i])
+print(halmaz)
 
-# Task 4 - find integers in list
+# Extend függvény
+# Feladat, adott két lista, füzzük össze őket
+lista1 = [1,2,3]
+lista2 = [4,5,6]   # -> [1,2,3,4,5,6]
 
-# Task 5 - largest gap in a list
+lista = lista1[:]
+lista.append(lista2)
+print(lista) # [1, 2, 3, [4, 5, 6]]
 
-# Task 6 - largest and second largest numbers
+lista = lista1[:]
+lista.extend(lista2) # Nem muszáj hogy a lista2 lista legyen 
+print(lista) # [1, 2, 3, 4, 5, 6]
 
-# Task 7 - sorting a list
+lista = lista1 + lista2 # Ha mind a kettő lista
+print(lista) # [1, 2, 3, 4, 5, 6]
+
+lista = [1,2, 3]
+lista.extend((4,5,6))
+lista.extend("kiskutya")
+print(lista)
+
+# print([1,2,3] + "kiskutya") ERROR
+
---
+++ ora13.py
@@ -0,0 +1,61 @@
+#Task 1 - find all the common values in the two lists
+list_1 = [61, 5, 6, 28, 87, 52, 89, 51, 86, 29, 93, 26, 99, 16, 36, 53, 47, 95, 18, 54, 62, 37, 34, 11, 75, 90, 88, 24, 72, 76, 55, 44,  3, 50, 35, 17, 94,  7, 31,100, 42, 43, 74, 83, 82,  4, 10]
+list_2 = [5, 9, 62, 79, 17, 68, 54, 50, 60, 89, 29, 41, 83, 77,  3, 86, 56, 13, 26, 52, 98, 81, 82, 74, 55, 66, 92, 61, 30, 37, 57, 91,  2, 71, 93, 35, 33, 24,100, 19, 65, 95, 90, 38, 88, 31, 80, 70, 25, 39, 15, 85, 42, 94, 11, 76, 32,  7, 48]
+
+
+#Task 2 - create one number from the list values
+numbers = [12, 54, 812]
+
+# Task 3 - make two lists the same
+list_1 = [43, 70, 25, 39, 15, 85, 42, 94, 11, 76, 20,  36, 48]
+list_2 = [ 36, 44, 20, 96, 69, 15, 27, 14, 87, 67, 97, 43,  8, 22]
+
+
+# Task 4 - is the list sorted / is the list strictly increasing
+list = [43, 70, 25, 39, 15, 85, 42, 94, 11, 76, 20,  36, 48]
+
+
+# Task 5 - price of items in a shop
+items = ["apple", "book", "bread", "cheese", "chicken", "curry sauce", "doughnut", "toilet roll", "socks", "toothpaste"]
+prices = [1500, 1000, 700, 1600, 1900, 600, 800, 999, 500, 550]
+
+# Task 6 - 100. Fibonacci number
+
+# Task 7 - 400m running results
+names = ["Bob", "Wanda", "Jared", "Emma", "Lisa", "Fred", "George", "Noah", "Rachel"]
+times = [123.42, 67.15, 80.70, 118.40, 99.95, 68.22, 71.51, 102.68, 80.88]
+
+
+# Task 8 - price of the translation
+string = """Mr. and Mrs. Dursley, of number four, Privet Drive, were proud to say that they were perfectly
+normal, thank you very much. They were the last people you’d expect to be involved in anything
+strange or mysterious, because they just didn’t hold with such nonsense.
+ Mr. Dursley was the director of a firm called Grunnings, which made drills. He was a big, beefy
+man with hardly any neck, although he did have a very large mustache. Mrs. Dursley was thin
+and blonde and had nearly twice the usual amount of neck, which came in very useful as she
+spent so much of her time craning over garden fences, spying on the neighbors. The Dursleys
+had a small son called Dudley and in their opinion there was no finer boy anywhere.
+ The Dursleys had everything they wanted, but they also had a secret, and their greatest fear was
+that somebody would discover it. They didn’t think they could bear it if anyone found out about
+the Potters. Mrs. Potter was Mrs. Dursley’s sister, but they hadn’t met for several years; in fact,
+Mrs. Dursley pretended she didn’t have a sister, because her sister and her good-for-nothing
+husband were as unDursleyish as it was possible to be. The Dursleys shuddered to think what the
+neighbors would say if the Potters arrived in the street. The Dursleys knew that the Potters had a
+small son, too, but they had never even seen him. This boy was another good reason for keeping
+the Potters away; they didn’t want Dudley mixing with a child like that.
+ When Mr. and Mrs. Dursley woke up on the dull, gray Tuesday our story starts, there was
+nothing about the cloudy sky outside to suggest that strange and mysterious things would soon be
+happening all over the country. Mr. Dursley hummed as he picked out his most boring tie for
+work, and Mrs. Dursley gossiped away happily as she wrestled a screaming Dudley into his high
+chair.
+ None of them noticed a large, tawny owl flutter past the window.
+ At half past eight, Mr. Dursley picked up his briefcase, pecked Mrs. Dursley on the cheek, and
+tried to kiss Dudley good-bye but missed, because Dudley was now having a tantrum and
+throwing his cereal at the walls. """
+
+# Task 9 - getting information from a person
+name = ["Bob", "Wanda", "Jared", "Emma", "Lisa", "Fred", "George", "Noah", "Rachel"]
+age = [26, 31, 35, 41, 58, 30, 46, 61, 25]
+gender = ["male", "female", "male", "female", "female", "male", "male", "male", "female"]
+job = ["web developer", "marketing director", "content creator", "human resources", "CEO", "software developer", "public relations manager", "tester", "sales representative"]
+salary = [1500, 1500, 1400, 1300, 1400, 1500, 1400, 1300, 1500]
