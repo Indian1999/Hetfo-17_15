5de3e9bf562beb6c09a9115937b08af5775ce6b7
Indian1999 <haszab99@gmail.com>
2026-05-18 17:19:04 +0200

    feladatok

---
+++ ora14.py
@@ -0,0 +1,11 @@
+# 1. feladat: Hány napos vagyok?
+
+# 2. feladat: Anagramma-e?
+
+# 3. feladat: Kik lassítsanak?
+speed = [96, 98, 72, 64, 93, 61, 95, 78, 54, 51, 52, 55, 47, 70, 68, 67, 79, 83, 59, 76, 45, 82, 87, 66, 89, 62, 69, 74, 75, 48, 88, 81, 86, 97, 94, 71, 46, 57, 50, 53]
+
+
+# 4. feladat: Szavazás eredménye
+burgers = ["Spicy Pinata", "Cheesy Dream", "Vegan Fluffy", "Fatty Boom", "Tortuga", "Pork Pie"]
+votes = [95061, 93439, 98563, 90478, 90915, 97334]
